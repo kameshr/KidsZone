b483fa64654d97d2fe80653a3db07fc382596c7a
Suyash Raghavndra and Aryash Raghavendra <kameshr@gmail.com>
2021-01-17 21:57:36 +0530

    Added quadratic solution to AlgebraCraft

--- src/AlgebraCraft.py
+++ src/AlgebraCraft.py
@@ -18,6 +18,9 @@ import sys
 from termcolor import colored
 import os
 from fractions import Fraction
+import math
+import cmath
+import sympy
 
 superscript = str.maketrans("0123456789", "\u2070\u00b9\u00b2\u00b3\u2074\u2075\u2076\u2077\u2078\u2079")
 
@@ -28,24 +31,29 @@ def exitCraft():
 
 # Print basic help message
 def printhelp():
-	print(colored("[INFO]", 'yellow', attrs=['reverse', 'bold']) + colored("Commands: binom <two numbers> <positive integer power>, sumsq <two numbers>, subsq <two numbers>, sumcb <two numbers>, subcb <two numbers>, exit", 'yellow'))
+	print(colored("[INFO]", 'yellow', attrs=['reverse', 'bold', 'blink']) + colored(" Commands: qs <three co-efficients of quadratic equation to solve>, binom <two numbers> <positive integer power>, sumsq <two numbers>, subsq <two numbers>, sumcb <two numbers>, subcb <two numbers>, exit", 'yellow'))
 
 # Function to verify command prompt argument numbers
 def checknum(cmd_args):
-	chk_args = ['', '']
+	chk_args = []
 	try:
 		chk_args[0] = int(cmd_args[1])
 		chk_args[1] = int(cmd_args[2])
+		if len(cmd_args) == 4:
+			chk_args[2] = int(cmd_args[3])
 	except:
 		try:
 			chk_args[0] = float(cmd_args[1])
 			chk_args[1] = float(cmd_args[2])
+			if len(cmd_args) == 4:
+				chk_args[2] = float(cmd_args[3])
 		except:
 			try:
 				chk_args[0] = Fraction(cmd_args[1])
 				chk_args[1] = Fraction(cmd_args[2])
+				if len(cmd_args) == 4:
+					chk_args[2] = Fraction(cmd_args[3])
 			except:
-				print(chk_args)
 				print(colored("[ERROR", 'red', attrs=['reverse', 'bold']) + colored(" Only integer, decimal and fraction numbers allowed.", 'red', attrs=['bold']))
 				printhelp()
 				return ["ERROR", "ERROR"]
@@ -98,6 +106,8 @@ def subcb(cmd_args):
 # Function to calculate binomial expansion
 def binom(cmd_args):
 	chk_args = checknum(cmd_args)
+	if chk_args[0] == "ERROR":
+		return 0
 	power = 0
 	try:
 		power = int(cmd_args[3])
@@ -142,6 +152,69 @@ def binom(cmd_args):
 	print(colored("(", op_color) + colored("a", var_color) + colored(" + ", op_color) + colored("b", var_color) + colored(")", op_color) + colored(str(power).translate(superscript), pow_color) + colored(" = ", op_color) + formula)
 	print(colored("Therefore, (", op_color) + colored(str(chk_args[0]), var_color) + colored(" + ", op_color) + colored(str(chk_args[1]), var_color) + colored(")", op_color) + colored(str(power).translate(superscript), pow_color) + colored(" = ", op_color) + colored(str(value), value_color))
 
+# Qudratic equation solution
+def quadsolve(cmd_args):
+	chk_args = [Fraction(), Fraction(), Fraction()]
+	try:
+		chk_args[0] = Fraction(cmd_args[1])
+		chk_args[1] = Fraction(cmd_args[2])
+		chk_args[2] = Fraction(cmd_args[3])
+	except:
+		print(colored("[ERROR]",'red',attrs=['bold', 'blink', 'reverse']) + colored(" Quadratic equation co-efficients 'a', 'b' and 'c' need to be integers, decimals or fractions only.", 'red'))
+		return 0
+	
+	var_color = 'blue'
+	pow_color = 'green'
+	op_color = 'yellow'
+	coeff_color = 'white'
+	value_color = 'white'
+
+	if chk_args[0] == 0:
+		print(colored("[ERROR]", 'red', attrs=['bold', 'blink', 'reverse']) + colored(" Not a quadratic equation.", 'red', attrs=['bold']))
+		return 0
+	
+	delta = pow(chk_args[1], 2) - 4*chk_args[0]*chk_args[2]
+	root1 = root2 = "Undefined"
+	equation = ''
+
+	if chk_args[0] < 0:
+		equation = colored("-", op_color) + colored(str(abs(chk_args[0])), coeff_color) + colored("x", var_color) + colored(str("2").translate(superscript), pow_color)
+	elif chk_args[0] == 1:
+		equation = colored("x", var_color) + colored(str("2").translate(superscript), pow_color)
+	else:
+		equation = colored(str(abs(chk_args[0])), coeff_color) + colored("x", var_color) + colored(str("2").translate(superscript), pow_color)
+	
+	if chk_args[1] < 0:
+		equation = equation + colored(" - ", op_color) + colored(str(abs(chk_args[1])), coeff_color) + colored("x", var_color)
+	elif chk_args[1] == 1:
+		equation = equation + colored(" + ", op_color) + colored("x", var_color)
+	elif chk_args[1] > 0:
+		equation = equation + colored(" + ", op_color) + colored(str(abs(chk_args[1])), coeff_color) + colored("x", var_color)
+
+	if chk_args[2] < 0:
+		equation = equation + colored(" - ", op_color) + colored(str(abs(chk_args[2])), coeff_color)
+	elif chk_args[2] > 0:
+		equation = equation + colored(" + ", op_color) + colored(str(abs(chk_args[2])), coeff_color)
+	
+	equation = equation + colored(" = ", op_color) + colored("0", coeff_color)
+
+	if delta < 0:
+		print(colored("[INFO]", 'yellow', attrs=['bold', 'blink', 'reverse']) + colored(" Roots are complex.", 'yellow'))
+		root1 = colored(str((-chk_args[1] + cmath.sqrt(delta))/(2*chk_args[0])), value_color, attrs=['bold', 'underline'])
+		root2 = colored(str(((-chk_args[1]) - cmath.sqrt(delta))/(2*chk_args[0])), value_color, attrs=['bold', 'underline'])
+	elif (math.sqrt(delta) - math.floor(math.sqrt(delta)) == 0): 
+		print(colored("[INFO]", 'yellow', attrs=['bold', 'blink', 'reverse']) + colored(" Roots are rational.", 'yellow'))
+		root1 = colored(str(Fraction((-chk_args[1] + math.sqrt(delta))/(2*chk_args[0]))), value_color, attrs=['bold', 'underline'])
+		root2 = colored(str(Fraction((-chk_args[1] - math.sqrt(delta))/(2*chk_args[0]))), value_color, attrs=['bold', 'underline'])
+	else:
+		print(colored("[INFO]", 'yellow', attrs=['bold', 'blink', 'reverse']) + colored(" Roots are irrational.", 'yellow'))
+		root1 = colored(str((-chk_args[1] + sympy.sqrt(delta))/(2*chk_args[0])), value_color, attrs=['bold', 'underline'])
+		root2 = colored(str((-sympy.sqrt(delta) - chk_args[1])/(2*chk_args[0])), value_color, attrs=['bold', 'underline'])
+	
+	print("Solutions to the equation " + equation + " are " + root1 + " and " + root2 + ".")
+
+	return 0
+
 # Function implementing the command prompt
 def cmdprompt():
 	os.system("clear")
@@ -177,6 +250,10 @@ def cmdprompt():
 
 		elif cmd_args[0] == "binom":
 			binom(cmd_args)
+			continue
+
+		elif cmd_args[0] == "qs":
+			quadsolve(cmd_args)
 			continue
 
 		else:
